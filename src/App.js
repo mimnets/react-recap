22b813f0179bb5cc96169b56ae2f5f5bfbd97c36
MiMNets <mdmonirulislammonir@gmail.com>
2020-08-22 00:33:17 +0600

    another component property pass to a current component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -15,6 +15,7 @@ function App() {
         <Nayok name={nayoks[0]}></Nayok>
         <Nayok></Nayok>
         <Nayok></Nayok>
+        <MovieCounter></MovieCounter>
       
       </header>
     </div>
@@ -23,6 +24,28 @@ function App() {
 
 // User defined component 
 // Component is case sensative : You must type the component name in Capital later, otherwise it will not work
+
+function MovieCounter(){
+  // const [first, second] = ['abc', 'def'] // Or we can write as below :
+  // const first = 'abc';
+  // const second = 'def';
+  // So we can write here :
+  const [count, setCount] = useState(1) // A function for state for storing data which may change useState() or we can call hook
+  const handleClick = () => setCount(count + 1)
+  
+
+  return(
+    <div>
+      <button onClick={handleClick}>Add Movie</button>
+      <h3>Number of movies : {count}</h3>
+      <MovieDisplay movies={count + 1}></MovieDisplay>
+    </div>
+  )
+}
+
+function MovieDisplay(props){
+return <h5>Movies I have acted : {props.movies}</h5>
+}
 
 function Nayok(props){ // Pass function parameter props as component property. We can use our own property anme or parameter name like : test instead of props
   const nayokStyle = {
@@ -38,7 +61,8 @@ function Nayok(props){ // Pass function parameter props as component property. W
   )
 }
 
-function MyComponent(){
+function MyComponent()
+{
   const myComponentStyle = { // We can declare style a component
     border: '2px solid white',
     margin: '5px 5px',
